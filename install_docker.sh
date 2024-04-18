d17c6f3d5fc43e1926d2e46c0a6f896b23d8fa59
lazzicat <jones184189@gmail.com>
2024-04-18 17:22:02 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# 更新 apt 包索引
-sudo apt update
-
+# Add Docker's official GPG key:
+sudo apt-get update
 # 安裝依賴包
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# 下載 Docker 的官方 GPG 密鑰並將其加入到 Docker 的 keyring
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/docker-archive-keyring.gpg >/dev/null
-
-# 設置 Docker 的穩定版儲存庫，並自動輸入 Enter 鍵
-echo | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null <<EOF
-deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
-EOF
-
-# 更新 apt 包索引
-sudo apt update
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
 # 安裝 Docker CE
 sudo apt install -y docker-ce
